84d91fcef5c1a13a05e29385bf7574863b2109b8
AOF <pongpich.nakages@gmail.com>
2023-07-12 17:25:28 +0700

    patient_card_kdms
    patient_card_kdms

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react"
+import Header from './components/Header'
+// import Icon5 from "../patient_card_header.png";
+// import logo from './patient_card_header.png';
+
+function App() {
+  return(
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App;
